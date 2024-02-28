375ed998ed5313c4317608f30cc81357d70eb363
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-02-28 14:54:46 +0100

    [end] primo esercizio

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-let test = Math.ceil(Math.random() * 10);
-console.log(test);
+const inpJs = document.getElementById("input");
+const btnJs = document.querySelector("button");
+const resultJs = document.getElementById("result")
+
+
+btnJs.addEventListener("click", function(){
+  
+  const wordCheck = inpJs.value;
+  palindro(wordCheck,resultJs);
+
+})
+
+function palindro(wordCheck, eIHTML) {
+
+  
+  if (wordCheck.length < 3) {
+
+    return eIHTML.innerHTML = "Inserisci una parola di almeno tre lettere."
+    
+  }
+
+  
+  const wardRevers = wordCheck.split("").reverse().join("");
+  return eIHTML.innerHTML = wordCheck === wardRevers ? "palindromo" : "non palindromo";
+ 
+}
